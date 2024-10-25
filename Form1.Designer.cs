66f4583c2d809c0c683d6d86cff09127ed90e427
tuba jabeen <92146433+TubaJabeen123@users.noreply.github.com>
2024-10-25 14:08:59 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace RemoteCar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Commands = new System.Windows.Forms.Label();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.Compile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(108, 121);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(258, 44);
+            this.Input.TabIndex = 0;
+            // 
+            // Commands
+            // 
+            this.Commands.AutoSize = true;
+            this.Commands.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Commands.Location = new System.Drawing.Point(33, 61);
+            this.Commands.Name = "Commands";
+            this.Commands.Size = new System.Drawing.Size(143, 31);
+            this.Commands.TabIndex = 1;
+            this.Commands.Text = "Commands";
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(108, 297);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(258, 96);
+            this.Output.TabIndex = 2;
+            this.Output.Text = "";
+            // 
+            // Compile
+            // 
+            this.Compile.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Compile.Location = new System.Drawing.Point(147, 213);
+            this.Compile.Name = "Compile";
+            this.Compile.Size = new System.Drawing.Size(146, 47);
+            this.Compile.TabIndex = 3;
+            this.Compile.Text = "Compile";
+            this.Compile.UseVisualStyleBackColor = true;
+            this.Compile.Click += new System.EventHandler(this.Compile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 36F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 465);
+            this.Controls.Add(this.Compile);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Commands);
+            this.Controls.Add(this.Input);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Label Commands;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.Button Compile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteCar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Compile_Click(object sender, EventArgs e)
+        {
+            // Define initial state and error states
+            string Initial_State = "Idle";
+            string Error_State = "Error";
+            string Unrecognized_Command_Error = "Unrecognized";
+
+            // Define dictionary for each valid command transition
+            var dict = new Dictionary<string, Dictionary<string, string>>
+            {
+                { "Idle", new Dictionary<string, string>
+                    {
+                        { "start", "Moving" },       // Car can only start from idle
+                        { "stop", "Idle" },          // Stop keeps it idle
+                        { "accelerate", Error_State }, // Can't accelerate if idle
+                        { "right", Error_State },      // Can't turn if idle
+                        { "left", Error_State }       // Unsupported command
+                    }
+                },
+                { "Moving", new Dictionary<string, string>
+                    {
+                        { "stop", "Idle" },          // Can stop to go idle
+                        { "accelerate", "Moving" },  // Can accelerate while moving
+                        { "right", "TurningRight" }, // Can turn right while moving
+                        { "left", Error_State }       // Unsupported command
+                    }
+                },
+                { "TurningRight", new Dictionary<string, string>
+                    {
+                        { "stop", "Idle" },          // Stop brings it back to idle
+                        { "start", "Moving" },       // Can start moving again
+                        { "accelerate", "Moving" },  // Can accelerate from turning
+                        { "right", "TurningRight" }, // Can continue turning right
+                        { "left", Error_State }       // Unsupported command
+                    }
+                }
+            };
+
+            // Define a set of all valid commands
+            var validCommands = new HashSet<string> { "start", "stop", "accelerate", "right", "left" };
+
+            // Capture and split input command sequence
+            string[] commands = Input.Text.ToLower().Split(' ');
+            string state = Initial_State;
+
+            // Process each command in sequence
+            foreach (string command in commands)
+            {
+                // Check if command is valid; if not, set state to Unrecognized_Command_Error
+                if (!validCommands.Contains(command))
+                {
+                    state = Unrecognized_Command_Error;
+                    break;
+                }
+
+                // Check for valid transitions
+                if (dict[state].ContainsKey(command))
+                {
+                    // Transition to next state based on command
+                    state = dict[state][command];
+                }
+                else
+                {
+                    // If command not allowed in the current state, mark as error
+                    state = Error_State;
+                    break;
+                }
+            }
+
+            // Display results based on final state
+            if (state == Error_State)
+            {
+                Output.Text = "ERROR: Command not valid in the current state.";
+            }
+            else if (state == Unrecognized_Command_Error)
+            {
+                Output.Text = "ERROR: Unrecognized command.";
+            }
+            else
+            {
+                Output.Text = "RESULT OKAY";
+            }
+
+            // Clear the input field after processing
+            Input.Text = string.Empty;
+        }
+    
+    }
+}
